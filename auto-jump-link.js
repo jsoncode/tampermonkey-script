a3bdf766fdee6933f8f2591df6e24df83d13936f
jsoncode <jsoncode@users.noreply.github.com>
2022-07-07 15:57:06 +0800

    第三方链接自动跳转-优化
    第三方链接自动跳转

--- auto-jump-link.js
+++ auto-jump-link.js
@@ -5,16 +5,18 @@
 // @description  try to take over the world!
 // @author       You
 // @match        **://**/**
-// @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
-    let hasUrl = /target|url/.test(location.search)
-    let hasText = /即将离开/.test(document.body.innerText)
-    let hasTextGo = /继续访问/.test(document.body.innerText)
-    if(hasUrl && hasText && hasTextGo){
-        //使用XPATH表达式查抄指定内容的dom元素
-        let a = document.evaluate("//a[contains(., '继续访问')]", document, null, XPathResult.ANY_TYPE).iterateNext();
-        a.click()
-    }
+    setTimeout(()=>{
+        let hasUrl = /target|url/.test(location.search)
+        let hasText = /即将离开/.test(document.body.innerText)
+        let hasTextGo = /继续/.test(document.body.innerText)
+        if(hasUrl && hasText && hasTextGo){
+            //使用XPATH表达式查抄指定内容的dom元素
+            let a = document.evaluate("//a[contains(., '继续')]", document, null, XPathResult.ANY_TYPE).iterateNext();
+            a.click()
+        }
+    },1000)
 })();
